5ef92eeddf5ff99a47570f7dae578db906ad34e9
Bonolond <dithatondl@gmail.com>
2022-10-13 18:12:56 -0700

    betty example

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main- Intro
+ * main - Entry point
  * Return: Always 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multi
-lingual puzzle");
+	puts("\"Programming is like building a multi lingual puzzle");
 	return (0);
+
 }
