bc55873356cfec717f980d802f2d024407008742
Bonolond <dithatondl@gmail.com>
2022-10-19 12:19:55 -0700

    alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,10 +6,10 @@
 
 int main(void)
 {
-	int num;
+	char la;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	for (la = 'z'; la >= 'a'; la--)
+		putchar(la);
 
 	putchar('\n');
 	return (0);
