bec46d228f0e4b05b390967242c841f57cb0ebd7
Bonolond <dithatondl@gmail.com>
2022-10-19 18:55:10 -0700

    print absolut interger value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an int
+ * @n: the int in question
+ * Return: the absolut value of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= o)
+		return (n);
+	else
+		return (-n);
+}
