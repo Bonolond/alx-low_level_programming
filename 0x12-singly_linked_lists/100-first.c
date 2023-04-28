b1251e43e7982b5532e59509d93f6140eb200171
Constance Bonolo Ndlovu <dithatondl@gmail,com>
2023-04-28 16:55:19 -0700

    correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
 void __attribute__ ((constructor)) beforemain()
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n")
+	printf("I bore my house upon my back!\n");
 }
