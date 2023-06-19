2ffd8be22a76d13c5cbba34e16769cd2dee4f802
Constance Bonolo Ndlovu <dithatondl@gmail,com>
2023-06-19 14:09:40 -0700

    task 0

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ * @c: the char to be checked
+ * Return: 1 if char is lower or uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
